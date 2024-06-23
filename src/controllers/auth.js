0670ac75b8c348cbd5171e12112f8ce28e10c15d
denis <deadkor26@gmail.com>
2024-06-23 21:18:23 +0300

    appv 1

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -10,8 +10,8 @@ import createHttpError from 'http-errors';
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
-  res.json({
-    status: 200,
+  res.status(201).json({
+    status: 201,
     message: 'Successfully registered a user!',
     data: user,
   });
