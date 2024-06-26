8ae8875faac112d98864162e9ea3596665abeb59
deadkor121 <143342877+deadkor121@users.noreply.github.com>
2024-06-26 15:13:24 +0300

    Update authenticate.js

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -6,40 +6,38 @@ export const authenticate = async (req, res, next) => {
   const authHeader = req.get('Authorization');
 
   if (!authHeader) {
-    next(createHttpError(401, 'Please provide Authorization header'));
-    return;
+    return next(createHttpError(401, 'Please provide Authorization header'));
   }
 
-  const bearer = authHeader.split(' ')[0];
-  const token = authHeader.split(' ')[1];
+  const [bearer, token] = authHeader.split(' ');
 
   if (bearer !== 'Bearer' || !token) {
-    next(createHttpError(401, 'Auth header should be of type Bearer'));
-    return;
+    return next(createHttpError(401, 'Auth header should be of type Bearer'));
   }
 
-  const session = await SessionsCollection.findOne({ accessToken: token });
+  try {
+    const session = await SessionsCollection.findOne({ accessToken: token });
 
-  if (!session) {
-    next(createHttpError(401, 'Session not found'));
-    return;
+    if (!session) {
+      return next(createHttpError(401, 'Session not found'));
+    }
+
+    const isAccessTokenExpired =
+      new Date() > new Date(session.accessTokenValidUntil);
+
+    if (isAccessTokenExpired) {
+      return next(createHttpError(401, 'Access token expired'));
+    }
+
+    const user = await UsersCollection.findById(session.userId);
+
+    if (!user) {
+      return next(createHttpError(401, 'User not found'));
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(createHttpError(500, 'Internal Server Error'));
   }
-
-  const isAccessTokenExpired =
-    new Date() > new Date(session.accessTokenValidUntil);
-
-  if (isAccessTokenExpired) {
-    next(createHttpError(401, 'Access token expired'));
-  }
-
-  const user = await UsersCollection.findById(session.userId);
-
-  if (!user) {
-    next(createHttpError(401));
-    return;
-  }
-
-  req.user = user;
-
-  next();
 };
