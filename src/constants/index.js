3ec0ae7e02c16234e99495076c52cbd30ed46056
deadkor121 <143342877+deadkor121@users.noreply.github.com>
2024-07-01 10:54:39 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,17 +3,13 @@ import path from 'node:path';
 
 export const ENV_VAR = {
   PORT: 'PORT',
-
   SMTP_HOST: 'SMTP_HOST',
   SMTP_PORT: 'SMTP_PORT',
   SMTP_USER: 'SMTP_USER',
   SMTP_PASSWORD: 'SMTP_PASSWORD',
   SMTP_FROM: 'SMTP_FROM',
-
   JWT_SECRET: 'JWT_SECRET',
-
   APP_DOMAIN: 'APP_DOMAIN',
-
   CLOUD_NAME: 'CLOUD_NAME',
   CLOUD_API_KEY: 'CLOUD_API_KEY',
   CLOUD_API_SECRET: 'CLOUD_API_SECRET',
@@ -26,8 +22,3 @@ export const SORT_ORDER = {
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
-
-// Экспортируем необходимые переменные
-export const CLOUD_NAME = process.env.CLOUD_NAME;
-export const CLOUD_API_KEY = process.env.CLOUD_API_KEY;
-export const CLOUD_API_SECRET = process.env.CLOUD_API_SECRET;
