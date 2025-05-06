a617146ee8ead1fa3bd0777d80b73eba6bd76316
deadkor121 <143342877+deadkor121@users.noreply.github.com>
2025-05-06 13:39:50 +0300

    test

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,6 +3,10 @@ import routerAuth from './auth.js';
 import routerContacts from './contacts.js';
 
 const rootRouter = Router();
+rootRouter.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the API' });
+});
+
 
 rootRouter.use('/auth', routerAuth);
 rootRouter.use('/contacts', routerContacts);
